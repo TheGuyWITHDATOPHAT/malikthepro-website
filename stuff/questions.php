c40f3bc40c529e24ce60dfe3a5ea607cabaebc68
TheGuyWITHDATOPHAT <maliksheriff444@gmail.com>
2024-06-30 12:15:58 -0500

    Create questions.php

---
+++ stuff/questions.php
@@ -0,0 +1,19 @@
+<html>
+  <head>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&display=swap" rel="stylesheet">
+    <style>
+      body {
+        font-family: 'Fredoka', sans-serif;
+      }
+      h1, h3 {
+        color: white;
+      }
+    </style>
+    <head>
+      <body>
+        <body style="background-color:#000000;">
+          <h1> Ask the man questions!!! <h1>
+      </body>
+</html>
